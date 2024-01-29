a6ad6405e03f6a92b3e4e3128c5bc72e30255d57
essium <wdm2008@gmail.com>
2024-01-29 10:58:37 +0800

    init commit

---
+++ src/main/java/org/dm/fin/ana/Main.java
@@ -0,0 +1,4 @@
+package org.dm.fin.ana;
+
+public class Main {
+}
---
+++ src/main/java/org/dm/fin/ana/utils/DateUtil.java
@@ -0,0 +1,21 @@
+package org.dm.fin.ana.utils;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+public class DateUtil {
+    public static DateTimeFormatter FMT_HYPHEN = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    public static DateTimeFormatter FMT_STANDARD = DateTimeFormatter.ofPattern("yyyyMMdd");
+
+    private static LocalDate date_orig = LocalDate.of(1990, 1, 1);
+
+    public static long date2Long(String dateStr, DateTimeFormatter fmt) {
+        LocalDate date = LocalDate.from(fmt.parse(dateStr));
+        return ChronoUnit.DAYS.between(date_orig, date);
+    }
+
+    public static String long2Date (long dateLong, DateTimeFormatter fmt) {
+        return date_orig.plusDays(dateLong).format(fmt);
+    }
+}
